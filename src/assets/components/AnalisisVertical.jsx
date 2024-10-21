3ec964a7ed533c0ade9b7bd46872e976fd938676
PA20003 <pa20003@ues.edu.sv>
2024-10-20 23:33:05 -0600

    estado vertical

--- src/assets/components/AnalisisVertical.jsx
+++ src/assets/components/AnalisisVertical.jsx
@@ -1,19 +1,30 @@
 import React, { useState } from "react";
-import '../css/IndicadoresFinancieros.css'; // Asegúrate de importar el CSS
+import '../css/Estado.css'; // Asegúrate de importar el mismo CSS
 
-function AnalisisVertical() {
+function AnalisisFinanciero() {
   const [añoSeleccionado, setAñoSeleccionado] = useState('');
   const [balanceData, setBalanceData] = useState(null);
+  const [estadoData, setEstadoData] = useState(null);
+  const [vista, setVista] = useState('balance'); // Por defecto, muestra el balance
 
   const handleAñoChange = (e) => {
     const año = e.target.value;
     setAñoSeleccionado(año);
 
+    // Cargar datos del balance general
     const balances = JSON.parse(localStorage.getItem('balances'));
     if (balances && balances[año]) {
       setBalanceData(balances[año]);
     } else {
       setBalanceData(null);
+    }
+
+    // Cargar datos del estado de resultados
+    const estados = JSON.parse(localStorage.getItem('estados'));
+    if (estados && estados[año]) {
+      setEstadoData(estados[año]);
+    } else {
+      setEstadoData(null);
     }
   };
 
@@ -30,7 +41,7 @@ function AnalisisVertical() {
     <div className="estado-container">
       <div className="centrar">
         <h4>Saram S.A de C.V.</h4>
-        <h4>Análisis Vertical del Balance General</h4>
+        <h4>{vista === 'balance' ? "Análisis Vertical del Balance General" : "Estado de Resultado"}</h4>
         <h5>Del 1 de enero hasta el 31 de diciembre del {añoSeleccionado}</h5>
         <h5>Cifras expresadas en miles de dólares de los Estados Unidos de América</h5>
       </div>
@@ -45,10 +56,18 @@ function AnalisisVertical() {
           <option value="2020">2020</option>
           <option value="2019">2019</option>
         </select>
+
+        <label>Seleccionar Vista:</label>
+        <select onChange={(e) => setVista(e.target.value)} value={vista}>
+          <option value="">-- Seleccionar Opcion --</option>
+          <option value="balance">Balance General</option>
+          <option value="estado">Estado de Resultado</option>
+        </select>
       </div>
 
-      {balanceData ? (
+      {vista === 'balance' && balanceData ? (
         <div>
+          {/* Aquí va el contenido del balance general */}
           <h4>ACTIVOS</h4>
           <h5>ACTIVO CORRIENTE</h5>
           <ul>
@@ -98,16 +117,48 @@ function AnalisisVertical() {
             <li>Reservas: {formatearNumero(balanceData.reservas)} ({calcularPorcentaje(balanceData.reservas, balanceData.totalPatrimonio)})</li>
             <li>Resultados Acumulados: {formatearNumero(balanceData.resultadosAcumulados)} ({calcularPorcentaje(balanceData.resultadosAcumulados, balanceData.totalPatrimonio)})</li>
             <li>Resultados del Ejercicio: {formatearNumero(balanceData.resultadosEjercicio)} ({calcularPorcentaje(balanceData.resultadosEjercicio, balanceData.totalPatrimonio)})</li>
-            <li>Ajustes y Efectos por Valuación y Cambio de Valor: {formatearNumero(balanceData.ajustesEfectosValuacion)} ({calcularPorcentaje(balanceData.ajustesEfectosValuacion, balanceData.totalPatrimonio)})</li>
-          </ul>
+            <li>Ajustes y Efectos por Valuación y Cambio de Valor: {formatearNumero(balanceData.ajustesEfectosValuacion)} ({calcularPorcentaje(balanceData.ajustesEfectosValuacion, balanceData.totalPatrimonio)})</li>          </ul>
           <h5>TOTAL PATRIMONIO: {formatearNumero(balanceData.totalPatrimonio)}</h5>
 
-          <h5>TOTAL PASIVO Y PATRIMONIO: {formatearNumero(balanceData.totalPasivosPatrimonio)}</h5>
+          <h4>TOTAL PASIVO + PATRIMONIO: {formatearNumero(balanceData.totalPasivos + balanceData.totalPatrimonio)}</h4>
+        </div>
+      ) : vista === 'estado' && estadoData ? (
+        <div>
+          {/* Aquí va el contenido del estado de resultados */}
+          <h4>INGRESOS</h4>
+          <ul>
+            <li>Ventas: {formatearNumero(estadoData.ventas)} ({calcularPorcentaje(estadoData.ventas, estadoData.ventas)})</li>
+            <li>Costo de Venta: {formatearNumero(estadoData.costoVenta)} ({calcularPorcentaje(estadoData.costoVenta, estadoData.ventas)})</li>
+            <li>Utilidad Bruta: {formatearNumero(estadoData.utilidadBruta)} ({calcularPorcentaje(estadoData.utilidadBruta, estadoData.ventas)})</li>
+          </ul>
+          <h4>GASTOS DE OPERACIÓN</h4>
+          <ul>
+            <li>Administración: {formatearNumero(estadoData.administracion)} ({calcularPorcentaje(estadoData.administracion, estadoData.ventas)})</li>
+            <li>Gerencia Financiera: {formatearNumero(estadoData.gerenciaFinanciera)} ({calcularPorcentaje(estadoData.gerenciaFinanciera, estadoData.ventas)})</li>
+            <li>Auditoría Interna: {formatearNumero(estadoData.auditoriaInterna)} ({calcularPorcentaje(estadoData.auditoriaInterna, estadoData.ventas)})</li>
+            <li>Gerencia Ventas y Mercadeo: {formatearNumero(estadoData.gerenciaVentasMercadeo)} ({calcularPorcentaje(estadoData.gerenciaVentasMercadeo, estadoData.ventas)})</li>
+            <li>División Avícola: {formatearNumero(estadoData.divisionAvicola)} ({calcularPorcentaje(estadoData.divisionAvicola, estadoData.ventas)})</li>
+            <li>Dirección: {formatearNumero(estadoData.direccion)} ({calcularPorcentaje(estadoData.direccion, estadoData.ventas)})</li>
+            <li>Cadena de Suministros: {formatearNumero(estadoData.cadenaSuministros)} ({calcularPorcentaje(estadoData.cadenaSuministros, estadoData.ventas)})</li>
+          </ul>
+          <h4>UTILIDAD DE OPERACIÓN: {formatearNumero(estadoData.utilidadOperacion)} ({calcularPorcentaje(estadoData.utilidadOperacion, estadoData.ventas)})</h4>
+          <h4>GASTOS NO OPERACIONALES</h4>
+          <ul>
+            <li>Gastos Financieros: {formatearNumero(estadoData.gastosFinancieros)} ({calcularPorcentaje(estadoData.gastosFinancieros, estadoData.ventas)})</li>
+            <li>Otros Gastos: {formatearNumero(estadoData.otrosGastosNoOperacionales)} ({calcularPorcentaje(estadoData.otrosGastosNoOperacionales, estadoData.ventas)})</li>
+          </ul>
+          <h5>Utilidad Antes de Impuestos y Reserva: {formatearNumero(estadoData.utilidadAntesImpuestosReserva)} ({calcularPorcentaje(estadoData.utilidadAntesImpuestosReserva, estadoData.ventas)})</h5>
+          <h5>Reserva Legal: {formatearNumero(estadoData.reservaLegal)} ({calcularPorcentaje(estadoData.reservaLegal, estadoData.ventas)})</h5>
+          <h4>UTILIDAD ANTES DE IMPUESTO: {formatearNumero(estadoData.utilidadAntesImpuesto)} ({calcularPorcentaje(estadoData.utilidadAntesImpuesto, estadoData.ventas)})</h4>
+          <h4>IMPUESTO SOBRE LA RENTA: {formatearNumero(estadoData.impuestoRenta)} ({calcularPorcentaje(estadoData.impuestoRenta, estadoData.ventas)})</h4>
+          <h4>CESC grandes contribuyentes: {formatearNumero(estadoData.cescGrandesContribuyentes)} ({calcularPorcentaje(estadoData.cescGrandesContribuyentes, estadoData.ventas)})</h4>
+          <h4>UTILIDAD DISTRIBUIBLE: {formatearNumero(estadoData.utilidadDistribuible)} ({calcularPorcentaje(estadoData.utilidadDistribuible, estadoData.ventas)})</h4>
         </div>
       ) : (
-        <p>Por favor selecciona un año para ver el análisis vertical.</p>
+        <p>Por favor selecciona un año y vista.</p>
       )}
     </div>
   );
 }
-export default AnalisisVertical;
+
+export default AnalisisFinanciero;
